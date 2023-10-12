c9421b91e835f86fa3b7d225fdf40f7a7777ad5b
Lesya <lesya91kami@gmail.com>
2023-10-12 20:50:42 +0300

    решали задачки

---
+++ task 3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №5. Напишите программу, которая на вход
+// принимает одно число (N), а на выходе показывает
+// все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+// System.Console.Write("Введите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int numberNegative = -number;
+// if (number < 0)
+// {
+//     System.Console.WriteLine("Введите положительное число");
+// }
+// else
+// {
+//     while (numberNegative <= number)
+//     {
+//         System.Console.Write($"{numberNegative} ");
+//         numberNegative++;
+//     }
+// }
+
+System.Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    number = -number;
+}
+int numberNegative = -number;
+while (numberNegative <= number)
+{
+    System.Console.Write($"{numberNegative} ");
+    numberNegative++;
+}
---
+++ task 4/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+
+    int digitLast = number % 10;
+    System.Console.Write(digitLast);
+}
+else
+{
+    System.Console.WriteLine("Введите трехзначное число");
+}
