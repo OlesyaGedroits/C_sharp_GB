81b6477b4a40a94fc3992ce6bb0f0ec502db0942
Lesya <lesya91kami@gmail.com>
2023-10-15 23:47:54 +0300

    задача 1 решена

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,2 +1,15 @@
-﻿int x = firstNumber;
+﻿int firstNumber = 5;
+int secondNumber = 6;
 
+if(firstNumber<secondNumber)
+{
+    System.Console.WriteLine($"Первое число {firstNumber} меньше чем второе число {secondNumber}");
+}
+else if(firstNumber>secondNumber)
+{
+    System.Console.WriteLine($"Первое число {firstNumber} больше чем второе число {secondNumber}");
+}
+else
+{
+    System.Console.WriteLine($"Введенные числа равны {firstNumber}");
+}
