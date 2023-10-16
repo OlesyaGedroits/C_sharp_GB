af74d0ba586f493e2e57bee9f3407e8cbf3405db
Lesya <lesya91kami@gmail.com>
2023-10-16 10:33:11 +0300

    решили вторую задачу

---
+++ DZ1/DZ1_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// int a = a;
+// int b = b;
+// int c = c;
+
+// int FindMax = a;
+
+// if(a>FindMax) FindMax = a;
+// if(b>FindMax) FindMax = b;
+// if(c>FindMax) FindMax = c;
+
+// Console.Write("FindMax = ");
+// Console.WriteLine(FindMax);
+
+System.Console.WriteLine("Введите три числа:");
+System.Console.Write("1. ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("2. ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("3. ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int FindMax = a;
+if(a>FindMax) FindMax = a;
+if(b>FindMax) FindMax = b;
+if(c>FindMax) FindMax = c;
+
+Console.Write("Наибольшее из трех введеных -> ");
+Console.WriteLine(FindMax);
