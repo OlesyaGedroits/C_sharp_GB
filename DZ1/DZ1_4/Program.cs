73f52fa7619426e4c35144a1e163b09f46b62883
Lesya <lesya91kami@gmail.com>
2023-10-16 12:06:51 +0300

    четвертая задача

---
+++ DZ1/DZ1_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// System.Console.Write("Введите целое положительно число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int second = 0;
+// if (number>second)
+// {
+//         System.Console.Write($"number = {number} -> ");
+// }
+// while (number>second)
+// {
+//     System.Console.Write($"{second+2}    ");
+//     second = second + 2;
+// }
+
+System.Console.Write("Введите целое положительно число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write($"{number} -> ");
+for (int second = 2; second <= number; second += 2)
+        {
+            Console.Write($"{second}\t");
+        }
