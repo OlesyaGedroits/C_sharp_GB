7bf7eeea67d3ec55128aec13e84051baba24f7a9
Lesya <lesya91kami@gmail.com>
2023-10-16 10:46:16 +0300

    решили третюю задачу

---
+++ DZ1/DZ1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число для проверки на четность: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int second = number % 2;
+if ( second == 0)
+{
+    System.Console.WriteLine($"Число `{number}` чётное");
+}
+else
+{
+    System.Console.WriteLine($"Число `{number}` нечётное");
+}
