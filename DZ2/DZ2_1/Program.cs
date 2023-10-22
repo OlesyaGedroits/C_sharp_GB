616070316690f1c312a06f894d05dec1c070c7f4
Lesya <lesya91kami@gmail.com>
2023-10-22 20:10:52 +0300

    домашка 1 задача

---
+++ DZ2/DZ2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// System.Console.WriteLine("Введите трехзначное число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int x1 = number%100;
+// int x3 = x1%10;
+// int x2 = (x1 - x3)/10;
+// System.Console.WriteLine($"-> {x2}");
+
+System.Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = {num};
+int x = array[1];
+System.Console.WriteLine(x);
