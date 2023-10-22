99f8499dcd25d54b2327f2e20953c04f0f7e7024
Lesya <lesya91kami@gmail.com>
2023-10-22 20:46:34 +0300

    домашка 2 задача

---
+++ DZ2/DZ2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+int chislo = Convert.ToInt32(num);
+if (chislo<100)
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    System.Console.WriteLine(num[2]);
+}
