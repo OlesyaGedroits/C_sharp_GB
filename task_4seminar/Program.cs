fd2c789151ae3b2330e89699e22fe6654f267498
Lesya <lesya91kami@gmail.com>
2023-10-23 20:27:26 +0300

    задания

---
+++ task_4seminar/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 24: Напишите программу, которая принимает
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int SumToN(int num)
+{
+    int sum = 1;
+    for (int i = 2; i <= num; i++)
+    {
+        sum += i; // == sum = sum + i
+    }
+    return sum;
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumToN(number);
+System.Console.WriteLine(result);
