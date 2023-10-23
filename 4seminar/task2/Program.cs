11cd8d8fb0e6f7a811c95368e9971823ef0ebdfd
Lesya <lesya91kami@gmail.com>
2023-10-23 20:41:20 +0300

    задания

---
+++ 4seminar/task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int quontaty (string num)
+{
+    int count = num.Length;
+    if (num[0]=='-')
+    {
+        count -= 1;
+    }
+
+return count;
+}
+
+System.Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+System.Console.WriteLine(quontaty(number));
