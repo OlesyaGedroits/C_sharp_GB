9e3e9d19e3a7e11c7a5489a9caafadcfa9fafa04
Lesya <lesya91kami@gmail.com>
2023-10-24 22:51:17 +0300

    решили дз задачу 3

---
+++ DZ3/DZ3_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Внутри класса Answer напишите метод ShowCube, который принимает на вход число (N)
+// и выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+
+int ShowCube(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        int cub = i*i*i;
+        System.Console.WriteLine(cub);
+    }
+
+}
+
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+????????
