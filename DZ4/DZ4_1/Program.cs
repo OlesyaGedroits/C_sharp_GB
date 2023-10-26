9655a50976c7adce3f193b15c09a0c998f981c5c
Lesya <lesya91kami@gmail.com>
2023-10-26 16:31:42 +0300

    решена 1ая задача ДЗ

---
+++ DZ4/DZ4_1/Program.cs
@@ -0,0 +1,30 @@
+﻿double Stepen (int a, int b)
+{
+    double result=1;
+    if (b==0)
+    {
+        return result;
+    }
+    else if (b>0)
+    {
+        for (int i = 1; i <= b; i++)
+        {
+            result=result*a;
+        }
+        return result;
+    }
+    else
+    {
+        for (int i = 1; i <= (b*-1); i++)
+        {
+            result=result*a;
+        }
+        return (1/result);
+    }
+}
+
+Console.WriteLine("Введите два число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите два число В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine ($"{a} в степени {b} = {Stepen(a, b)}");
