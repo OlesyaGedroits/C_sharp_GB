3a936e3e98d1a57f252e4e6fd0aa076070fbe379
Lesya <lesya91kami@gmail.com>
2023-10-26 18:10:33 +0300

    решена 2ая задача ДЗ

--- DZ4/DZ4_1/Program.cs
+++ DZ4/DZ4_1/Program.cs
@@ -1,4 +1,8 @@
-﻿double Stepen (int a, int b)
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double Stepen (int a, int b)
 {
     double result=1;
     if (b==0)
@@ -22,7 +26,6 @@
         return (1/result);
     }
 }
-
 Console.WriteLine("Введите два число А: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите два число В: ");
---
+++ DZ4/DZ4_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11 // 82 -> 10 // 9012 -> 12
+
+int SummaCifr (int number)
+{
+    int sum = 0;
+    if (number<0)
+    {
+        number=number*-1;
+    }
+        while (number>0)
+        {
+            int x=number%10;
+            number=number/10;
+            sum = sum+x; 
+        }
+    return sum;
+}
+Console.WriteLine("Введите чиало: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(SummaCifr(number));
+
