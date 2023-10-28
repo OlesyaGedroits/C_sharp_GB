ffb6c7a10589b44dd66b9ca1c3945d680b66201c
Lesya <lesya91kami@gmail.com>
2023-10-28 16:42:06 +0300

    решили третью задачу ДЗ4

--- DZ4/DZ4_3/Program.cs
+++ DZ4/DZ4_3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int[] PrintArray (int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        mas[i]=new Random().Next(-100, 101);
+    }
+    return mas;
+}
+
+int PrintMSG (string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] array = new int[PrintMSG("Введите количество элеметов массива: ")];
+System.Console.WriteLine($"[{string.Join(", ", PrintArray(array))}]");
+
+
