3998d4bfa8883e3d636442db468ea6ba2a1d1787
Lesya <lesya91kami@gmail.com>
2023-11-13 19:50:18 +0300

    первая задача 6ого семинара

--- 6seminar/1task/Program.cs
+++ 6seminar/1task/Program.cs
@@ -3,3 +3,35 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
+int[] FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 10);
+    }
+
+    return arr;
+}
+
+int GetNumber(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] ReverseArray(int[] arr)
+{
+    int temp = 0;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[arr.Length - 1 - i];
+        arr[arr.Length - 1 - i] = temp;
+    }
+    return arr;
+}
+
+int[] array = new int[GetNumber("Введите кол-во элементов массива: ")];
+array = FillArray(array);
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+System.Console.WriteLine($"[{string.Join(", ", ReverseArray(array))}]");
