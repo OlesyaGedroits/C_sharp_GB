cbe7f1359110555c453bd3f04e06e34830ebc4d6
Lesya <lesya91kami@gmail.com>
2023-11-13 20:49:02 +0300

    3 задача 6 семинара

--- 6seminar/3task/Program.cs
+++ 6seminar/3task/Program.cs
@@ -4,3 +4,22 @@
 // 3  -> 11
 // 2  -> 10
 
+
+int side(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetBinaryView(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    GetBinaryView(number / 2);
+    System.Console.Write(number % 2);
+}
+
+int number = side("Введите число: ");
+GetBinaryView(number);
