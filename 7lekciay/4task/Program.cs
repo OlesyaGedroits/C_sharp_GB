684b31233639c64a96d0ec795ea26d3f6c24c212
Lesya <lesya91kami@gmail.com>
2023-11-20 13:54:29 +0300

    задача из буткемпа

---
+++ 7lekciay/4task/Program.cs
@@ -0,0 +1,30 @@
+﻿// Как посмотреть содержимое папки?
+
+//смотрим файлы в папке и дату создания
+// string adress = "E:/GeekBreans/C#/4seminar/task2";
+// DirectoryInfo di = new DirectoryInfo(adress);
+// System.Console.WriteLine(di.CreationTime);
+// FileInfo[] fi =di.GetFiles();
+
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     System.Console.WriteLine(fi[i].Name);
+// }
+
+//смотрим все папки и файлы в них
+
+void CatalogInfo(string path, string indent = "")
+{
+ DirectoryInfo catalogs = new DirectoryInfo(path);
+ foreach (var currentCatalog in catalogs.GetDirectories())
+ {
+ Console.WriteLine($"{indent}{currentCatalog.Name}");
+ CatalogInfo(currentCatalog.FullName, indent + " ");
+ }
+ foreach (var item in catalogs.GetFiles())
+ {
+ Console.WriteLine($"{indent}{item.Name}");
+ }
+}
+string path = "E:/GeekBreans/C#/4seminar/task2";
+CatalogInfo(path);
